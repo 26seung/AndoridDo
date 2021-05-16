b2af82dbafbe2d6ed70037d95f772fbc81c02f74
26seung <yousi7@naver.com>
2021-05-17 03:19:26 +0900

    Room(liveData)

--- roomexam/src/main/java/com/example/roomexam/MainActivity.java
+++ roomexam/src/main/java/com/example/roomexam/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.roomexam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.room.Room;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mTodoEditText;
@@ -23,13 +26,18 @@ public class MainActivity extends AppCompatActivity {
         // 데이터베이스 객채 생성
         final AppDatabase db = Room.databaseBuilder(this,AppDatabase.class,"todo-db").allowMainThreadQueries().build();
 
-        mResultTextView.setText(db.todoDao().getAll().toString());
+        // UI 갱신
+        db.todoDao().getAll().observe(this, todos -> {
+            mResultTextView.setText(todos.toString());
 
+        });
+
+        // 버튼 클릭시 DB 에 insert
         findViewById(R.id.add_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-                mResultTextView.setText(db.todoDao().getAll().toString());
+//                mResultTextView.setText(db.todoDao().getAll().toString());
             }
         });
     }
